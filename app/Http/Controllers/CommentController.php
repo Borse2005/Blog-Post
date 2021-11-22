ee3df1fa9997cf8ca4b77a09228d52e3e522d34a
Darshan Santosh Borse <borsedarshan77@gmail.com>
2021-11-22 10:38:26 +0530

    Initial Commit

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -5,8 +5,6 @@ namespace App\Http\Controllers;
 use App\Http\Requests\Comment;
 use App\Models\Comment as ModelsComment;
 use App\Models\Post;
-use App\Models\User;
-use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
